23c284008187ffabdd5a4a86e841465e44096bd2
Dhruv Mehta <39973939+Dhruvmehta18@users.noreply.github.com>
2020-06-06 17:54:37 +0530

    movie detail page starter code

--- movieweb/urls.py
+++ movieweb/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movie.urls', namespace='movie')),
+    path('', include('movieDetail.urls', namespace='movie_detail')),
     path('', include('login.urls', namespace='login'))
 ]
