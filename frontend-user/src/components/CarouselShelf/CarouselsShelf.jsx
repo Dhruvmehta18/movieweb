b16e9026200d401b933185932787bc4b2639b285
Dhruv Mehta <39973939+Dhruvmehta18@users.noreply.github.com>
2020-12-12 17:20:54 +0000

    implemented scrolling feature of carousels

--- frontend-user/src/components/CarouselShelf/CarouselsShelf.jsx
+++ frontend-user/src/components/CarouselShelf/CarouselsShelf.jsx
@@ -1,34 +1,60 @@
-import React from "react";
-import { Box, IconButton, makeStyles } from "@material-ui/core";
+import React, { useRef } from "react";
+import { Box, Button, IconButton, makeStyles } from "@material-ui/core";
 import { ChevronLeft, ChevronRight } from "@material-ui/icons";
 import { LOADED } from "../../constants/constants";
 import CarouselsShelfHeader from "./CarouselsShelfHeader/CarouselsShelfHeader";
 import CarouselsShelfItems from "./CarouselsShelfItems/CarouselsShelfItems";
 import "./carouselsShelf.css";
+import PaperIconWrapper from "../PaperIconWrapper/PaperIconWrapper";
 const useStyles = makeStyles((theme) => ({
   navShelfIconsContainer: {
     position: "absolute",
-    top: "calc((var(--movie-card-height) + 2 * var(--movie-card-margin-vertical)) / 2)",
+    top:
+      "calc((var(--movie-card-height) + 2 * var(--movie-card-margin-vertical)) / 2)",
     transform: "translate3d(-50%, -50%, 0)",
     borderRadius: "50%",
-    zIndex: 2
+    zIndex: 2,
   },
-  navShelfIcon:{
-    color: theme.palette.common.black,
-    background: theme.palette.common.white,
-    zIndex: "inherit"
+  navShelfIcon: {
+    width: "var(--icon-button-width)",
+    minWidth: "var(--icon-button-width)",
+    height: "var(--icon-button-width)",
+    zIndex: "inherit",
+    borderRadius: "50%",
+    boxShadow: theme.shadows[6]
   },
-  prevIconContainer:{
-    left: "var(--carousels-shelf-icons-position)"
+  prevIconContainer: {
+    left: "var(--carousels-shelf-icons-position)",
   },
-  nextIconContainer:{
-    right: 0
-  }
+  nextIconContainer: {
+    right: 0,
+  },
 }));
 const CarouselsShelf = (props) => {
   const classes = useStyles();
+  const carouselContainerRef = useRef(null)
   const { carouselsList = [] } = props;
-  console.log(carouselsList);
+  const getScrollContainerValue = () => {
+    var style = getComputedStyle(document.body);
+    const bodyWidth = parseInt(style.width);
+    const movieCardBaseWidth = parseInt(style.getPropertyValue('--movie-card-width'));
+    const appSpacing = parseInt(style.getPropertyValue('--app-spacing'));
+    const movieCardMarginEnd = parseInt(style.getPropertyValue('--movie-card-margin-end'));
+    const movieCardWidth = movieCardBaseWidth + movieCardMarginEnd;
+    const cardPossible = Math.floor((bodyWidth-appSpacing) / movieCardWidth);
+    return cardPossible * movieCardWidth;
+  }
+
+  const onPrevButtonClicked = () => {
+    if(carouselContainerRef) {
+      carouselContainerRef.current.scrollLeft -= getScrollContainerValue()}
+  }
+  
+  const onNextButtonClicked = () => {
+    if(carouselContainerRef){
+      getScrollContainerValue()
+       carouselContainerRef.current.scrollLeft += getScrollContainerValue()}
+  }
   return (
     <Box>
       {carouselsList &&
@@ -38,16 +64,42 @@ const CarouselsShelf = (props) => {
             <Box key={index}>
               <CarouselsShelfHeader title={carousel.title} />
               <Box position="relative">
-                <Box className={[classes.navShelfIconsContainer, classes.prevIconContainer].join(" ")}>
-                  <IconButton aria-label="previous" className={classes.navShelfIcon}>
-                    <ChevronLeft />
-                  </IconButton>
+                <Box
+                  className={[
+                    classes.navShelfIconsContainer,
+                    classes.prevIconContainer,
+                  ].join(" ")}
+                >
+                  <PaperIconWrapper>
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      aria-label="previous"
+                      className={classes.navShelfIcon}
+                      onClick={onPrevButtonClicked}
+                    >
+                      <ChevronLeft />
+                    </Button>
+                  </PaperIconWrapper>
                 </Box>
-                <CarouselsShelfItems carousel={carousel} />
-                <Box className={[classes.navShelfIconsContainer, classes.nextIconContainer].join(" ")}>
-                  <IconButton aria-label="next" className={classes.navShelfIcon}>
-                    <ChevronRight />
-                  </IconButton>
+                <CarouselsShelfItems carousel={carousel} carouselRef={carouselContainerRef}/>
+                <Box
+                  className={[
+                    classes.navShelfIconsContainer,
+                    classes.nextIconContainer,
+                  ].join(" ")}
+                >
+                  <PaperIconWrapper>
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      aria-label="next"
+                      className={classes.navShelfIcon}
+                      onClick={onNextButtonClicked}
+                    >
+                      <ChevronRight />
+                    </Button>
+                  </PaperIconWrapper>
                 </Box>
               </Box>
             </Box>
--- frontend-user/src/components/CarouselShelf/CarouselsShelfItems/CarouselsShelfItems.jsx
+++ frontend-user/src/components/CarouselShelf/CarouselsShelfItems/CarouselsShelfItems.jsx
@@ -5,10 +5,10 @@ import CarouselsShelfItem from './CarouselsShelfItem/CarouselsShelfItem';
 import "./Carouselsitems.css"
 
 const CarouselsShelfItems = props => {
-    const {carousel} = props
+    const {carousel, carouselRef} = props
     return (
         <Box width="100%">
-        <Grid container wrap="nowrap" className="carousels-container">
+        <Grid container wrap="nowrap" className="carousels-container" ref={carouselRef}>
             {carousel.list.map((movie, index)=>{
                 return (
                     <CarouselsShelfItem key={movie.id} movie={movie}/>
