84b608be7d8c39cb55af1241aab0b9db6199959b
Dhruv Mehta <39973939+Dhruvmehta18@users.noreply.github.com>
2020-12-13 11:07:09 +0000

    implemented dynamic images

--- frontend-user/src/components/CarouselShelf/CarouselsShelfItems/CarouselsShelfItem/CarouselsShelfItem.jsx
+++ frontend-user/src/components/CarouselShelf/CarouselsShelfItems/CarouselsShelfItem/CarouselsShelfItem.jsx
@@ -1,6 +1,6 @@
 import { Grid, Paper } from "@material-ui/core";
 import React, { memo } from "react";
-import Image from "../../../Image/Image";
+import DynamicImage from "../../../DynamicImage/DynamicImage";
 import "./carousels_shelf_item.css";
 
 const CarouselsShelfItem = memo((props) => {
@@ -11,12 +11,13 @@ const CarouselsShelfItem = memo((props) => {
   return (
     <Grid item component={Paper} className="movie-card" elevation={4}>
       <a href={linkTo} className="text-reset">
-        <Image
+        <DynamicImage
           className="card-img-top card-image"
           alt={movie.title}
-          srcSet={srcset}
-          src={src}
           dataSrc={src}
+          dataSrcset={srcset}
+          fallbackImageUrl={movie.card_photo.small.download_url}
+          isFallbackBlur
         />
       </a>
     </Grid>
